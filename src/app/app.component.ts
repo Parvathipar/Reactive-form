8fc885aced250a55751c612ff1497aefd5ce4a9e
Your Parvathi <parvathigvaikom.@gmail.com>
2024-04-05 19:09:46 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'reactive-form';
+  Form=new FormGroup({
+    name:new FormControl(''),
+    author:new FormControl(''),
+    publication:new FormControl(''),
+    price:new FormControl(''),
+    genre:new FormControl(''),
+    date:new FormControl(''),
+    country:new FormControl(''),
+    description:new FormControl('')
+  })
+  save(){
+    console.log(this.Form.value)
+  }
 }
